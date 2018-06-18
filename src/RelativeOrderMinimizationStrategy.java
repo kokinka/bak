9ac3df45ed8a1cbf6f0b268ebbde2aa1e6522a19
kokinka <jarka.kokavcova@szm.sk>
2018-06-18 12:32:50 +0200

    bugfix
    v poradi musia byt vsetky chromozomy z unstable node

--- src/RelativeOrderMinimizationStrategy.java
+++ src/RelativeOrderMinimizationStrategy.java
@@ -58,6 +58,11 @@ public class RelativeOrderMinimizationStrategy implements MinimizationStrategy {
         for (int i = 0; i < stableNode.chromosomes.size(); i++) {
             dfs(stableNode, i, 0, unstableNode);
         }
+
+        for (int i = 0; i < unstableNode.chromosomes.size(); i++){
+            if (!spracovane.get(1).get(i))
+                poradie.add(i);
+        }
     }
 
     private void relatback(EvolutionTree.EvolutionNode stableNode, EvolutionTree.EvolutionNode unstableNode) {
@@ -73,6 +78,10 @@ public class RelativeOrderMinimizationStrategy implements MinimizationStrategy {
         }
         for (int i = 0; i < stableNode.chromosomes.size(); i++) {
             dfsback(stableNode, i, 1, unstableNode);
+        }
+        for (int i = 0; i < unstableNode.chromosomes.size(); i++){
+            if (!spracovane.get(0).get(i))
+                poradie.add(i);
         }
 
     }
